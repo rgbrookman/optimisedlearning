8a6340e180056a71f8bf8c72626252fe814cbafd
rgbrookman <rgbrookman19@gmail.com>
2025-05-25 13:09:16 +0100

    vercel errors 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { TranscriptProvider } from "@/app/contexts/TranscriptContext";
 import { EventProvider } from "@/app/contexts/EventContext";
+import { Suspense } from 'react';
 import App from "./App";
 
 export default function Page() {
   return (
+    <Suspense fallback={<div>Loading page content...</div>}>
     <TranscriptProvider>
       <EventProvider>
         <App />
       </EventProvider>
     </TranscriptProvider>
+    </Suspense>
   );
 }
